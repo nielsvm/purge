d89d8cb0e3daa2a237ee11e9bb417ac7e13098c2
Niels van Mourik <niels.vanmourik@acquia.com>
2014-03-20 12:17:32 +0100

    QueueServiceInterface now provides API friendly queue methods that allow the API user to easily add, claim, release and delete purgeables without accessing the underlying queue (JUST STUBS, UNIMPLEMENTED). QueueServiceInterface now also depends on a injected PurgeableFactoryInterface.

--- lib/Drupal/purge/Queue/QueueService.php
+++ lib/Drupal/purge/Queue/QueueService.php
@@ -8,6 +8,8 @@
 namespace Drupal\purge\Queue;
 
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\purge\Purgeable\PurgeableFactoryInterface;
+use Drupal\purge\Purgeable\PurgeableInterface;
 use Drupal\purge\Queue\QueueInterface;
 
 /**
@@ -23,10 +25,151 @@ class QueueService extends ServiceProviderBase implements QueueServiceInterface 
   private $queue;
 
   /**
+   * The factory that generates purgeable objects on the fly.
+   *
+   * @var \Drupal\purge\Purgeable\PurgeableFactoryInterface
+   */
+  private $purgeable_factory;
+
+  /**
+   * The transaction buffer used to park purgeable objects.
+   */
+  private $buffer;
+
+  /**
    * {@inheritdoc}
    */
-  function __construct(QueueInterface $queue) {
+  function __construct(QueueInterface $queue, PurgeableFactoryInterface $purgeable_factory) {
+    $this->purgeable_factory = $purgeable_factory;
     $this->queue = $queue;
+
+    // The queue service attempts to collect all actions done for purgeables
+    // in $this->buffer, and commits them as infrequent as possible during
+    // runtime. At minimum it will commit to the underlying queue plugin upon
+    // shutdown and by doing so, attempts to reduce and bundle the amount of
+    // work the queue has to do (e.g., queries, disk writes, mallocs). This
+    // helps purge to scale better and should cause no noticeable side-effects.
+    register_shutdown_function(array($this, 'commit'));
+  }
+
+  /**
+   * Add a purgeable to the queue, schedule it for later purging.
+   *
+   * @param \Drupal\purge\Purgeable\PurgeableInterface $purgeable
+   *   A purgeable describes a single item to be purged and can be created using
+   *   the 'purge.purgeable_factory'. The object instance added to the queue can
+   *   be claimed and executed by the 'purge.purger' service later.
+   */
+  public function add(PurgeableInterface $purgeable) {
+
+  }
+
+  /**
+   * Add multiple purgeables to the queue, schedule them for later purging.
+   *
+   * @param array $purgeables
+   *   A non-associative array with \Drupal\purge\Purgeable\PurgeableInterface
+   *   objects to be added to the queue. The purgeables can later be claimed
+   *   from the queue and fed to the 'purge.purger' executor.
+   */
+  public function addMultiple(array $purgeables) {
+
+  }
+
+  /**
+   * Claims a purgeable from the queue for immediate purging.
+   *
+   * @param $lease_time
+   *   The lease time determines how long the processing is expected to take
+   *   place in seconds, defaults to an hour. After this lease expires, the item
+   *   will be reset and another consumer can claim the purgeable. Very short
+   *   lease times can result in purgeables being purged twice by parallel
+   *   processes, due this inefficiency the one-hour default is recommended for
+   *   most purgers.
+   *
+   * @return \Drupal\purge\Purgeable\PurgeableInterface
+   *   Returned will be a fully instantiated purgeable object or FALSE when the
+   *   queue is empty. Be aware that its expected that the claimed item needs
+   *   to be fed to the purger within the specified $lease_time, else they will
+   *   become available again.
+   */
+  public function claim($lease_time = 3600) {
+
+  }
+
+  /**
+   * Claim multiple purgeables for immediate purging from the queue at once.
+   *
+   * @param $claims
+   *   Determines how many claims at once should be claimed from the queue. When
+   *   the queue is unable to return as many items as requested it will return
+   *   as much items as it can.
+   * @param $lease_time
+   *   The lease time determines how long the processing is expected to take
+   *   place in seconds, defaults to an hour. After this lease expires, the item
+   *   will be reset and another consumer can claim the purgeable. Very short
+   *   lease times can result in purgeables being purged twice by parallel
+   *   processes, due this inefficiency the one-hour default is recommended for
+   *   most purgers.
+   *
+   * @return array
+   *   Returned will be a non-associative array with the given amount of
+   *   \Drupal\purge\Purgeable\PurgeableInterface objects as claimed. Be aware
+   *   that its expected that the claimed purgeables will need to be processed
+   *   by the purger within the given $lease_time, else they will become
+   *   available again. The returned array might be empty when the queue is.
+   */
+  public function claimMultiple($claims = 10, $lease_time = 3600) {
+
+  }
+
+  /**
+   * Release a purgeable that couldn't be purged, back to the queue.
+   *
+   * @param \Drupal\purge\Purgeable\PurgeableInterface $purgeable
+   *   The purgeable that couldn't be held for longer or that failed processing,
+   *   to be marked as free for processing in the queue. Once released, other
+   *   consumers can claim and attempt purging it again.
+   */
+  public function release(PurgeableInterface $purgeable) {
+
+  }
+
+  /**
+   * Release purgeables that couldn't be purged, back to the queue.
+   *
+   * @param array $purgeables
+   *   A non-associative array with \Drupal\purge\Purgeable\PurgeableInterface
+   *   objects to released and marked as available in the queue. Once released,
+   *   other consumers can claim them again and attempt purging them.
+   */
+  public function releaseMultiple(array $purgeables) {
+
+  }
+
+  /**
+   * Delete a purged purgeable from the queue.
+   *
+   * @param \Drupal\purge\Purgeable\PurgeableInterface $purgeable
+   *   The purgeable that was successfully purged and that should be removed
+   *   from the queue. The object instance might remain to exist but should not
+   *   be accessed anymore, cleanup might occur later during runtime.
+   */
+  public function delete(PurgeableInterface $purgeable) {
+
+  }
+
+  /**
+   * Delete multiple purgeables from the queue at once.
+   *
+   * @param array $purgeables
+   *   A non-associative array with \Drupal\purge\Purgeable\PurgeableInterface
+   *   objects to be removed from the queue. Once called, the instance might
+   *   still exists but should not be accessed anymore, cleanup might occur
+   *   later during runtime.
+   */
+  public function deleteMultiple(array $purgeables) {
+
   }
 
   /**
@@ -34,5 +177,43 @@ class QueueService extends ServiceProviderBase implements QueueServiceInterface 
    */
   function emptyQueue() {
     $this->queue->deleteQueue();
+  }
+
+  /**
+   * Commit all actions in the internal buffer to the queue.
+   */
+  public function commit() {
+    if (empty($this->buffer)) {
+      return;
+    }
+    die(__METHOD__);
+  }
+
+  /**
+   * Commit all adding purgeables in the buffer to the queue.
+   */
+  private function commitCreating() {
+    die(__METHOD__);
+  }
+
+  /**
+   * ????.
+   */
+  private function commitClaiming() {
+    die(__METHOD__);
+  }
+
+  /**
+   * Commit all releasing purgeables in the buffer to the queue.
+   */
+  private function commitReleasing() {
+    die(__METHOD__);
+  }
+
+  /**
+   * Commit all deleting purgeables in the buffer to the queue.
+   */
+  private function commitDeleting() {
+    die(__METHOD__);
   }
 }
--- lib/Drupal/purge/Queue/QueueServiceInterface.php
+++ lib/Drupal/purge/Queue/QueueServiceInterface.php
@@ -10,6 +10,8 @@ namespace Drupal\purge\Queue;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
 use Drupal\Core\DependencyInjection\ServiceModifierInterface;
+use Drupal\purge\Purgeable\PurgeableFactoryInterface;
+use Drupal\purge\Purgeable\PurgeableInterface;
 use Drupal\purge\Queue\QueueInterface;
 
 /**
@@ -22,8 +24,114 @@ interface QueueServiceInterface extends ServiceProviderInterface, ServiceModifie
    *
    * @param \Drupal\purge\Queue\QueueInterface $queue
    *   The queue plugin which the service interacts with.
+   * @param \Drupal\purge\Purgeable\PurgeableFactoryInterface $purgeable_factory
+   *   The factory service to generate purgeable objects with.
    */
-  function __construct(QueueInterface $queue);
+  function __construct(QueueInterface $queue, PurgeableFactoryInterface $purgeable_factory);
+
+  /**
+   * Add a purgeable to the queue, schedule it for later purging.
+   *
+   * @param \Drupal\purge\Purgeable\PurgeableInterface $purgeable
+   *   A purgeable describes a single item to be purged and can be created using
+   *   the 'purge.purgeable_factory'. The object instance added to the queue can
+   *   be claimed and executed by the 'purge.purger' service later.
+   */
+  public function add(PurgeableInterface $purgeable);
+
+  /**
+   * Add multiple purgeables to the queue, schedule them for later purging.
+   *
+   * @param array $purgeables
+   *   A non-associative array with \Drupal\purge\Purgeable\PurgeableInterface
+   *   objects to be added to the queue. The purgeables can later be claimed
+   *   from the queue and fed to the 'purge.purger' executor.
+   */
+  public function addMultiple(array $purgeables);
+
+  /**
+   * Claims a purgeable from the queue for immediate purging.
+   *
+   * @param $lease_time
+   *   The lease time determines how long the processing is expected to take
+   *   place in seconds, defaults to an hour. After this lease expires, the item
+   *   will be reset and another consumer can claim the purgeable. Very short
+   *   lease times can result in purgeables being purged twice by parallel
+   *   processes, due this inefficiency the one-hour default is recommended for
+   *   most purgers.
+   *
+   * @return \Drupal\purge\Purgeable\PurgeableInterface
+   *   Returned will be a fully instantiated purgeable object or FALSE when the
+   *   queue is empty. Be aware that its expected that the claimed item needs
+   *   to be fed to the purger within the specified $lease_time, else they will
+   *   become available again.
+   */
+  public function claim($lease_time = 3600);
+
+  /**
+   * Claim multiple purgeables for immediate purging from the queue at once.
+   *
+   * @param $claims
+   *   Determines how many claims at once should be claimed from the queue. When
+   *   the queue is unable to return as many items as requested it will return
+   *   as much items as it can.
+   * @param $lease_time
+   *   The lease time determines how long the processing is expected to take
+   *   place in seconds, defaults to an hour. After this lease expires, the item
+   *   will be reset and another consumer can claim the purgeable. Very short
+   *   lease times can result in purgeables being purged twice by parallel
+   *   processes, due this inefficiency the one-hour default is recommended for
+   *   most purgers.
+   *
+   * @return array
+   *   Returned will be a non-associative array with the given amount of
+   *   \Drupal\purge\Purgeable\PurgeableInterface objects as claimed. Be aware
+   *   that its expected that the claimed purgeables will need to be processed
+   *   by the purger within the given $lease_time, else they will become
+   *   available again. The returned array might be empty when the queue is.
+   */
+  public function claimMultiple($claims = 10, $lease_time = 3600);
+
+  /**
+   * Release a purgeable that couldn't be purged, back to the queue.
+   *
+   * @param \Drupal\purge\Purgeable\PurgeableInterface $purgeable
+   *   The purgeable that couldn't be held for longer or that failed processing,
+   *   to be marked as free for processing in the queue. Once released, other
+   *   consumers can claim and attempt purging it again.
+   */
+  public function release(PurgeableInterface $purgeable);
+
+  /**
+   * Release purgeables that couldn't be purged, back to the queue.
+   *
+   * @param array $purgeables
+   *   A non-associative array with \Drupal\purge\Purgeable\PurgeableInterface
+   *   objects to released and marked as available in the queue. Once released,
+   *   other consumers can claim them again and attempt purging them.
+   */
+  public function releaseMultiple(array $purgeables);
+
+  /**
+   * Delete a purged purgeable from the queue.
+   *
+   * @param \Drupal\purge\Purgeable\PurgeableInterface $purgeable
+   *   The purgeable that was successfully purged and that should be removed
+   *   from the queue. The object instance might remain to exist but should not
+   *   be accessed anymore, cleanup might occur later during runtime.
+   */
+  public function delete(PurgeableInterface $purgeable);
+
+  /**
+   * Delete multiple purgeables from the queue at once.
+   *
+   * @param array $purgeables
+   *   A non-associative array with \Drupal\purge\Purgeable\PurgeableInterface
+   *   objects to be removed from the queue. Once called, the instance might
+   *   still exists but should not be accessed anymore, cleanup might occur
+   *   later during runtime.
+   */
+  public function deleteMultiple(array $purgeables);
 
   /**
    * Empty the entire queue and reset all statistics.

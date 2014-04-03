ce7108028b43230050eec3b153a77bc7aa121e0c
Niels van Mourik <niels.vanmourik@acquia.com>
2014-04-03 09:58:28 +0200

    ServiceBase was not abstract and did not implement ServiceInterface, while it should.

--- lib/Drupal/purge/ServiceBase.php
+++ lib/Drupal/purge/ServiceBase.php
@@ -8,9 +8,10 @@
 namespace Drupal\purge;
 
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\purge\ServiceInterface;
 
 /**
  * Provides a generic service for all DIC-registered service classes by Purge.
  */
-class ServiceBase extends ServiceProviderBase {
+abstract class ServiceBase extends ServiceProviderBase implements ServiceInterface {
 }

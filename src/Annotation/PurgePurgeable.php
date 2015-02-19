2842d36a8d4e93296df01fe7669343ce6a64dde7
Niels van Mourik <niels.vanmourik@acquia.com>
2015-02-19 16:28:21 +0100

    PURGEABLE API CLEANUP: purgeably types now explicitly declare if they need an expression or not.

--- src/Annotation/PurgePurgeable.php
+++ src/Annotation/PurgePurgeable.php
@@ -40,4 +40,23 @@ class PurgePurgeable extends Plugin {
    * @var \Drupal\Core\Annotation\Translation
    */
   public $description;
+
+  /**
+   * Whether purgeable objects of this type require a string expression that
+   * describes what needs to be purged. If put to FALSE, the fact this type is
+   * instantiated is deemed enough information for purgers to execute it.
+   *
+   * @var bool
+   */
+  public $expression_required = TRUE;
+
+  /**
+   * When expression_required = TRUE, this determines whether a string
+   * expression can be equal to "" (empty string). If FALSE, this purgeable type
+   * can not be instantiated with empty expression strings.
+   *
+   * @var bool
+   */
+  public $expression_can_be_empty = FALSE;
+
 }

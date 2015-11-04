402062b58b422d6c909b00165fea875ced2ec2f0
Niels van Mourik <niels.vanmourik@acquia.com>
2015-11-04 11:46:25 +0100

    Ported \Drupal\purge\Tests\CacheabilityHeadersTest to KernelTestBase

--- src/Tests/CacheabilityHeadersTest.php
+++ src/Tests/CacheabilityHeadersTest.php
@@ -7,20 +7,40 @@
 
 namespace Drupal\purge\Tests;
 
-use Drupal\purge\Tests\WebTestBase;
+use Drupal\purge\Tests\KernelTestBase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Tests purge.services.yml enabling the X-Drupal-Cache-Tags response header.
  *
  * @group purge
  */
-class CacheabilityHeadersTest extends WebTestBase {
+class CacheabilityHeadersTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['system'];
+
+  /**
+   * {@inheritdoc}
+   */
+  function setUp() {
+    parent::setUp();
+    $this->installSchema('system', ['router']);
+    \Drupal::service('router.builder')->rebuild();
+  }
+
+  /**
+   * Test header presence on a frontpage request.
+   */
   public function testHeaderPresence() {
-    $this->drupalGet('');
-    $this->assertEqual($this->drupalGetHeader('X-Drupal-Cache'), 'MISS');
-    $header = $this->drupalGetHeader('X-Drupal-Cache-Tags');
-    $this->assertTrue(is_string($header), 'X-Drupal-Cache-Tags present and a string.');
-    $this->assertFalse(empty($header), 'X-Drupal-Cache-Tags is not empty.');
+    $request = Request::create('/');
+    $response = \Drupal::getContainer()->get('http_kernel')->handle($request);
+    $this->assertFalse(is_null($response->headers->get('X-Drupal-Cache-Tags')));
+    $this->assertTrue(is_string($response->headers->get('X-Drupal-Cache-Tags')));
+    $this->assertTrue(strlen($response->headers->get('X-Drupal-Cache-Tags')));
   }
 
 }

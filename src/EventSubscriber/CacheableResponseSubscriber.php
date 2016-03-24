1d257f35ef36c62cef4e65378dd2f7ce16417ea2
Niels van Mourik <niels.vanmourik@acquia.com>
2016-03-24 18:55:27 +0100

    [header-plugins] Ported CacheableResponseSubscriber to new plugin architecture, updated test.

--- src/EventSubscriber/CacheableResponseSubscriber.php
+++ src/EventSubscriber/CacheableResponseSubscriber.php
@@ -11,18 +11,29 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\Core\Cache\CacheableResponseInterface;
+use Drupal\purge\Plugin\Purge\TagsHeader\TagsHeadersServiceInterface;
 
 /**
- * Adds the Purge-Cache-Tags response header, to aid external caching systems.
+ * Add cache tags headers on cacheable responses, for external caching systems.
  */
 class CacheableResponseSubscriber implements EventSubscriberInterface {
 
   /**
-   * The name of the cache tags header to send.
+   * The tagsheaders service for iterating the available header plugins.
    *
-   * @var string
+   * @var \Drupal\purge\Plugin\Purge\TagsHeader\TagsHeadersServiceInterface
    */
-  const HEADER = 'Purge-Cache-Tags';
+  protected $purgeTagsHeaders;
+
+  /**
+   * Constructs a CacheableResponseSubscriber object.
+   *
+   * @param \Drupal\purge\Plugin\Purge\TagsHeader\TagsHeadersServiceInterface $purge_tagsheaders
+   *   The tagsheaders service for iterating the available header plugins.
+   */
+  public function __construct(TagsHeadersServiceInterface $purge_tagsheaders) {
+    $this->purgeTagsHeaders = $purge_tagsheaders;
+  }
 
   /**
    * {@inheritdoc}
@@ -33,7 +44,7 @@ class CacheableResponseSubscriber implements EventSubscriberInterface {
   }
 
   /**
-   * Sets the Purge-Cache-Tags header on cacheable responses.
+   * Add cache tags headers on cacheable responses.
    *
    * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
    *   The event to process.
@@ -43,13 +54,23 @@ class CacheableResponseSubscriber implements EventSubscriberInterface {
       return;
     }
 
-    // First ensure that ::getCacheableMetadata() exists on the object.
+    // Only set any headers when this is a cacheable response.
     $response = $event->getResponse();
     if ($response instanceof CacheableResponseInterface) {
 
-      // Only proceed setting the tags when it isn't yet set by other modules.
+      // Iterate all tagsheader plugins and add a header for each plugin.
       $tags = $response->getCacheableMetadata()->getCacheTags();
-      $response->headers->set(SELF::HEADER, implode(' ', $tags));
+      foreach ($this->purgeTagsHeaders as $header) {
+
+        // Retrieve the header name perform a few simple sanity checks.
+        $name = $header->getHeaderName();
+        if ((!is_string($name)) || empty(trim($name))) {
+          $plugin_id = $header->getPluginId();
+          throw new \LogicException("Header plugin '$plugin_id' should return a non-empty string on ::getHeaderName()!");
+        }
+
+        $response->headers->set($name, $header->getValue($tags));
+      }
     }
   }
 

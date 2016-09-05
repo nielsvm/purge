6bc966175cb9cb411bdbb2c0c854b5dd9fb8b8bb
Niels van Mourik <niels.vanmourik@acquia.com>
2016-09-05 17:56:37 +0200

    Tests\Queue\PluginTestBase now tests the paging logic of queue plugins.

--- src/Tests/Queue/PluginTestBase.php
+++ src/Tests/Queue/PluginTestBase.php
@@ -186,4 +186,81 @@ abstract class PluginTestBase extends KernelTestBase {
     $this->queue->deleteQueue();
   }
 
+  /**
+   * Test the paging behavior.
+   */
+  function testPaging() {
+    $this->assertEqual(0, $this->queue->numberOfItems());
+    // Assert that setting the paging limit, gets reflected properly.
+    $this->assertEqual($this->queue->selectPageLimit(), 15);
+    $this->assertEqual($this->queue->selectPageLimit(37), 37);
+    $this->assertEqual($this->queue->selectPageLimit(), 37);
+    $this->assertEqual($this->queue->selectPageLimit(7), 7);
+    $this->assertEqual($this->queue->selectPageLimit(), 7);
+    // Assert that an empty queue, results in no pages at all.
+    $this->assertEqual($this->queue->selectPageMax(), 0);
+    $this->assertEqual($this->queue->selectPage(), []);
+    // Create 25 items, which should be 3,5 (so 4) pages of 7 items each.
+    $this->assertIdentical(25, count($this->queue->createItemMultiple([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])));
+    $this->assertEqual($this->queue->selectPageMax(), 4);
+    $this->assertEqual($this->queue->selectPageLimit(5), 5);
+    $this->assertEqual($this->queue->selectPageMax(), 5);
+    $this->assertEqual($this->queue->selectPageLimit(7), 7);
+    // Test the data in the first page, omit the page parameter which is 1.
+    $page_1 = $this->queue->selectPage();
+    $this->assertEqual(count($page_1), 7);
+    $this->assertEqual($page_1[0]->item_id, 1);
+    $this->assertEqual($page_1[1]->item_id, 2);
+    $this->assertEqual($page_1[2]->item_id, 3);
+    $this->assertEqual($page_1[3]->item_id, 4);
+    $this->assertEqual($page_1[4]->item_id, 5);
+    $this->assertEqual($page_1[5]->item_id, 6);
+    $this->assertEqual($page_1[6]->item_id, 7);
+    $this->assertEqual($page_1[0]->data, 1);
+    $this->assertEqual($page_1[1]->data, 2);
+    $this->assertEqual($page_1[2]->data, 3);
+    $this->assertEqual($page_1[3]->data, 4);
+    $this->assertEqual($page_1[4]->data, 5);
+    $this->assertEqual($page_1[5]->data, 6);
+    $this->assertEqual($page_1[6]->data, 7);
+    $this->assertEqual($page_1[0]->expire, 0);
+    $this->assertEqual($page_1[1]->expire, 0);
+    $this->assertEqual($page_1[2]->expire, 0);
+    $this->assertEqual($page_1[3]->expire, 0);
+    $this->assertEqual($page_1[4]->expire, 0);
+    $this->assertEqual($page_1[5]->expire, 0);
+    $this->assertEqual($page_1[6]->expire, 0);
+    // Test the second page, which should be 7 items.
+    $page_2 = $this->queue->selectPage(2);
+    $this->assertEqual(count($page_2), 7);
+    $this->assertEqual($page_2[0]->data, 8);
+    $this->assertEqual($page_2[1]->data, 9);
+    $this->assertEqual($page_2[2]->data, 10);
+    $this->assertEqual($page_2[3]->data, 11);
+    $this->assertEqual($page_2[4]->data, 12);
+    $this->assertEqual($page_2[5]->data, 13);
+    $this->assertEqual($page_2[6]->data, 14);
+    // Test the third page, which should be 7 items as well.
+    $page_3 = $this->queue->selectPage(3);
+    $this->assertEqual(count($page_3), 7);
+    $this->assertEqual($page_3[0]->data, 15);
+    $this->assertEqual($page_3[1]->data, 16);
+    $this->assertEqual($page_3[2]->data, 17);
+    $this->assertEqual($page_3[3]->data, 18);
+    $this->assertEqual($page_3[4]->data, 19);
+    $this->assertEqual($page_3[5]->data, 20);
+    $this->assertEqual($page_3[6]->data, 21);
+    // The last page, should only be 4 items in total.
+    $page_4 = $this->queue->selectPage(4);
+    $this->assertEqual(count($page_4), 4);
+    $this->assertEqual($page_4[0]->data, 22);
+    $this->assertEqual($page_4[1]->data, 23);
+    $this->assertEqual($page_4[2]->data, 24);
+    $this->assertEqual($page_4[3]->data, 25);
+    // And obviously, there should be no fifth page.
+    $this->assertEqual($this->queue->selectPage(5), []);
+
+    $this->queue->deleteQueue();
+  }
+
 }
